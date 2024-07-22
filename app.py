81df5127246ef586b5d95b6f9eacd43ec5a775a7
Eli Hammond <ebaal8226@gmail.com>
2024-07-22 09:18:19 +0000

    add session user profile

--- app.py
+++ app.py
@@ -20,8 +20,6 @@ mongo = PyMongo(app)
 
 @app.route("/")
 @app.route("/get_tasks")
-
-
 def get_tasks():
     tasks = mongo.db.tasks.find()
     return render_template("tasks.html", tasks=tasks)
@@ -47,6 +45,7 @@ def register():
         # put user into session cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration Successfull")
+        return redirect(url_for("profile", username=session["user"]))
 
     return render_template("register.html")
 
@@ -63,7 +62,10 @@ def login():
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(request.form.get("username")))
+                    flash("Welcome, {}".format(
+                        request.form.get("username")))
+                    return redirect(url_for(
+                        "profile", username=session["user"]))
             else:
                 #invalid password
                 flash("Incorrect Username and/or Password")
@@ -76,6 +78,13 @@ def login():
             
     return render_template("login.html")
 
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    #get session user's username from the db
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
